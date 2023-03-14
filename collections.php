e4f2e08e737827f6680f4a02b858af55e02849c7
AaronTweeton <aaron@aarontweeton.com>
2023-03-14 04:10:18 -0700

    Initial commit.

---
+++ collections.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+ * Plugin Name:       Collections
+ * Description:       Manage collections of historical archives, for either family or community. 
+ * Version:           0.1.0
+ * Requires at least: 6.1
+ * Requires PHP:      8.0
+ * Author:            Aaron Tweeton
+ * Author URI:        https://aarontweeton.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       collections
+ * Domain Path:       /languages
+ */
